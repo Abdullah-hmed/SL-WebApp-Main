25a040e5ffd26164e8e419b8177f1057049f068f
Abdullah-hmed <abdullahwyi@gmail.com>
2024-12-19 19:58:35 +0500

    setup vite for frontend dev

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,87 @@
+import express from 'express';
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const router = express.Router();
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+// SIGN UP
+router.post('/signup', async (req, res) => {
+    const {username, email, password} = req.body;
+
+    try {
+
+        const { data: authData, error: authError } = await supabase.auth.signUp({
+            email, password
+        });
+
+        if (authError) {
+            console.error('Auth Error:', authError);
+            return res.status(400).json({ error: authError.message });
+        }
+
+        const userId = authData.user.id;
+
+        const { error: dbError } = await supabase.from('users').insert([
+            { id: userId, username },
+        ]);
+
+        if (dbError) {
+            console.error('Database Error:', dbError);
+            return res.status(400).json({ error: dbError.message });
+        }
+
+        res.status(201).json({ message: 'User created successfully' });
+
+    } catch (err) {
+        console.error('Server Error:', err);
+        res.status(500).json({ message: 'Server Error!' });
+    }
+});
+
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+
+        const { data: authData, error: authError } = await supabase.auth.signInWithPassword({
+            email, password
+        });
+
+        if (authError) {
+            console.error('Auth Error:', authError);
+            return res.status(400).json({ error: authError.message });
+        } 
+
+        return res.status(200).json({ message: 'User logged in!' });
+
+    } catch (err) {
+        console.error('Server Error:', err);
+        return res.status(500).json({ message: 'Server Error!'});
+
+    }
+});
+
+// SIGN OUT
+router.post('/signout', async (req, res) => {
+    try {
+
+        await supabase.auth.signOut();
+        return res.status(200).json({ message: 'User signed out!' });
+
+    } catch (err) {
+        console.error('Server Error:', err);
+        return res.status(500).json({ message: "Server Error!"});
+
+    }
+});
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,16 @@
-import express from 'express'
+import dotenv from 'dotenv';
+import express from 'express';
+import authRoutes from './controllers/authController.js';
 
-const app = express()
 
-app.listen(5000, () => {
-    console.log("App listening on port 5000")
-})
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
