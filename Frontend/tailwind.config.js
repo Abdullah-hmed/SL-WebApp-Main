aff02bb9b1b3c73803266d4bd4229de8cb04e269
Abdullah-hmed <abdullahwyi@gmail.com>
2024-12-23 01:09:01 +0500

    cleanup

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,21 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        appear: {
-          "0%": {
-            opacity: "0",
-          },
-          "100%": {
-            opacity: "1",
-          },
-        },
-        animation: {
-          appear: "appear 0.5s ease-in-out",
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
