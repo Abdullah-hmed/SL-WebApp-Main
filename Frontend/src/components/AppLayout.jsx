bc8115a19302534f18860ce6f627a4fe144e9873
Abdullah-hmed <abdullahwyi@gmail.com>
2024-12-23 01:09:01 +0500

    cleanup

--- Frontend/src/components/AppLayout.jsx
+++ Frontend/src/components/AppLayout.jsx
@@ -5,18 +5,10 @@ import LearnScreen from './Screens/Learn.jsx';
 import QuizScreen from './Screens/Quiz.jsx';
 import AccountScreen from './Screens/Account.jsx';
 import { 
-  Flame, 
-  Heart,
-  Trophy,
   Book,
   Gamepad,
   User,
   Home,
-  CheckCircle,
-  Lock,
-  Crown,
-  Star,
-  Users,
   HandHeart
 } from 'lucide-react';
 
@@ -51,7 +43,7 @@ const BottomIcons = ({activeButton, setActiveButton}) => {
     
     return (
         <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-purple-100 shadow-lg">
-            <div className="max-w-lg mx-auto px-4 py-1">
+            <div className="max-w-4xl mx-auto px-4 py-1">
                 <div className="flex justify-around items-center">
                     {BottomButtons.map((item) => {
                         const Icon = item.icon;
@@ -59,7 +51,8 @@ const BottomIcons = ({activeButton, setActiveButton}) => {
                         return (
                             <button 
                                 key={item.name}
-                                className={`flex flex-col items-center gap-1 px-4 py-1 transition-all duration-300 rounded-xl ${isActive ? 'scale-105' : ''} `}
+                                className={`flex flex-col items-center gap-1 px-4 py-1 w-full
+                                        hover:bg-gray-100 transition-all duration-300 rounded-xl ${isActive ? 'scale-105' : ''} `}
                                 onClick={() =>handleButtonClick(item.name)}
                                 >
                                 <Icon className={`text-2xl ${isActive ? 'text-purple-600' : 'text-gray-600'} transition-transform duration-200`} />
@@ -99,11 +92,11 @@ const BottomBar = ({activeButton, setActiveButton}) => {
 
 
 function AppLayout () {
-    const [activeButton, setActiveButton] = useState('Learn');
+    const [activeButton, setActiveButton] = useState('Home');
 
     return (
         <div className="min-h-screen bg-gradient-to-b from-blue-50 to-purple-50 pb-20 select-none">
-            <TopBar setActiveButton={setActiveButton} />
+            {/* <TopBar setActiveButton={setActiveButton} /> */}
             {chooseComponent(activeButton)}
             <BottomBar activeButton={activeButton} setActiveButton={setActiveButton} />
         </div>
