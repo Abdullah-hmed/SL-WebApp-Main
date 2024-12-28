b494e8505255483869ad3e2c2f46f5d402a84934
Abdullah-hmed <abdullahwyi@gmail.com>
2024-12-29 01:45:45 +0500

    styled accounts page

--- Frontend/src/components/Screens/Account.jsx
+++ Frontend/src/components/Screens/Account.jsx
@@ -1,6 +1,6 @@
 import { isAuthenticated, logoutUser, clearToken, getUserData, refreshUserData } from "../utils/authUtils";
 import { useEffect, useState } from 'react';
-
+import { UserCircle, RefreshCw, LogOut } from 'lucide-react';
 
 import { useNavigate } from "react-router-dom";
 
@@ -14,32 +14,49 @@ function AccountScreen () {
     const userData = getUserData();
     
     return (
-        <div className='flex flex-col items-center justify-center mx-2'>
-            <div className='max-w-screen-md flex flex-col items-center justify-center'>
-                <h2>Account</h2>
-                <div className="my-4">
+        <div className='min-h-screen bg-gray-50 flex items-center justify-center p-4'>
+            <div className='w-full max-w-md bg-white rounded-xl shadow-xl p-6'>
+                <UserCircle className="w-16 h-16 mx-auto text-purple-600" />
+                <h2 className="text-center font-semibold text-2xl text-gray-800">Account Information</h2>
+                <div className="space-y-1 mb-8">
                     {userData ? (
                         <>
-                            <p>Name: <span className="font-semibold">{userData.username}</span></p>
-                            <p>Email: <span className="font-semibold">{userData.email}</span></p>
+                        <div className="bg-gray-50 p-2 rounded-lg">
+                            <p className="text-gray-600 mb-2">
+                            Name
+                            <span className="block text-lg font-semibold text-gray-900">
+                                {userData.username}
+                            </span>
+                            </p>
+                        </div>
+                        <div className="bg-gray-50 p-2 rounded-lg">
+                            <p className="text-gray-600 mb-2">
+                            Email
+                            <span className="block text-lg font-semibold text-gray-900">
+                                {userData.email}
+                            </span>
+                            </p>
+                        </div>
                         </>
                     ) : (
-                        <p>No user data found.</p>
+                        <div className="text-center py-8">
+                        <p className="text-gray-500">No user data found.</p>
+                        </div>
                     )}
                 </div>
                 <button 
-                    className="px-4 py-3 mb-4 bg-purple-600 hover:bg-purple-500
-                                    text-white w-full rounded-lg transition-all disabled:bg-gray-400"
+                    className="flex items-center justify-center gap-2 px-4 py-3 mb-4 hover:bg-purple-600 bg-purple-50 text-purple-600
+                    border border-purple-600 hover:text-white w-full rounded-lg transition-all disabled:bg-gray-400"
                     onClick={() => refreshUserData()}
                 >
-                    Refresh Data
+                    <RefreshCw className="hover:animate-spin" /> Refresh Data
                 </button>
                 <button 
-                    className="px-4 py-3 bg-purple-600 hover:bg-purple-500
+                    className="flex items-center justify-center gap-2 px-4 py-3 bg-purple-600 hover:bg-purple-500
                                     text-white w-full rounded-lg transition-all disabled:bg-gray-400"
                     onClick={() => logoutUser(navigate)}
                 >
-                    Log Out
+                    <LogOut /> Log Out
                 </button>
             </div>
         </div>
